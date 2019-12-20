45174f96cadd3cb7a4fe4d56787e9ebcffc0b469
Daivan Trinh <daivan@gmail.com>
2019-12-21 00:55:36 +0100

    First commit

---
+++ classes/BoardGenerator.py
@@ -0,0 +1,17 @@
+def GenerateEmpties(string):
+    #print("Hello World")
+    return 'Here are your empties'
+
+def GenerateConstants(string):
+    #print("Hello World")
+    return 'Here are your constants'
+
+def GenerateNewKey(oldKey):
+    #print("Hello World")
+    return 'New key'
+
+def applyKeyToConstants(newKey,constants):
+    return 'new board'
+
+def isBoardValid(board):
+    return True
