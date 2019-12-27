8f9e2c591e55c81070e9255b8c7c576135092801
Daivan Trinh <daivan@gmail.com>
2019-12-28 00:44:06 +0100

    Can complete easy

--- main.py
+++ main.py
@@ -10,17 +10,18 @@ from functions import *
 # Very Easy
 #board = '901300000000250068068004000023060040007030010086010057619043072040597000070000400'
 # Easy
-board = '200000000070502938008000000320000000700050400004200013002407089009020604400901052'
-
+#board = '200000000070502938008000000320000000700050400004200013002407089009020604400901052'
+# Medium
+board = '030600040940075000600038092050000000800006070000000438380024000004000320070009084'
 #newBoard = singlePossibilityRule(board)
 
 newBoard = fullSinglePossibilityRule(board)
 newBoard = fullHiddenSingle(newBoard)
 
 print(newBoard)
-for index,n in enumerate(newBoard):
-    if n == '0':
-        options = getOptionsByIndex(newBoard, index)
+#for index,n in enumerate(newBoard):
+#    if n == '0':
+#        options = getOptionsByIndex(newBoard, index)
         #print(options)
         #print(index, n)
 
