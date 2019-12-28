98ce2b7356fd6d5d2ec6d74611ef7d81e0dc4708
Daivan Trinh <daivan@gmail.com>
2019-12-28 23:47:08 +0100

    Finished

--- main.py
+++ main.py
@@ -4,21 +4,23 @@
 #from array import *
 #import classes.BoardGenerator as BoardGenerator
 from functions import *
+from solver import *
 
-
+#testBoard = '090000000000000000000000000000000000000000000000000000000000000000000000000000000'
 
 # Very Easy
-#board = '901300000000250068068004000023060040007030010086010057619043072040597000070000400'
+#testBoard = '901300000000250068068004000023060040007030010086010057619043072040597000070000400'
 # Easy
-#board = '200000000070502938008000000320000000700050400004200013002407089009020604400901052'
+#testBoard = '200000000070502938008000000320000000700050400004200013002407089009020604400901052'
 # Medium
-board = '030600040940075000600038092050000000800006070000000438380024000004000320070009084'
+testBoard = '030600040940075000600038092050000000800006070000000438380024000004000320070009084'
 #newBoard = singlePossibilityRule(board)
-isBoardPossible(board)
-newBoard = fullSinglePossibilityRule(board)
-newBoard = fullHiddenSingle(newBoard)
 
-cleanBoard = newBoard
+#isBoardPossible(board)
+testBoard = fullSinglePossibilityRule(testBoard)
+testBoard = fullHiddenSingle(testBoard)
+
+#cleanBoard = newBoard
 
 # start with clean board
 # Get the pointer
@@ -32,10 +34,53 @@ cleanBoard = newBoard
     # Go back
     # If no more, update pointer
 
-for index,n in enumerate(newBoard):
-    if n == '0':
-        options = getOptionsByIndex(newBoard, index)
-        print(index, options)
-        sum = sum * len(options)
+#allOptions=[]
+#pointer = 0
+#action = 'Init'
 
-print('solutions: ',sum)
+
+solve(testBoard)
+
+#while pointer < 0:
+#while pointer<80:
+
+#    if board[pointer]!='0':
+#        print('skipping')
+#        pointer += 1
+#        continue
+
+#    print(board[pointer])
+#    options = getOptionsByIndex(board, pointer)
+#    print(options)
+#    pointer += 1
+    #if(board[pointer]!='0'):
+    #    pointer += 1
+    #    continue
+    #else:
+    #    print(pointer)
+    #    options = getOptionsByIndex(board, pointer)
+    #    for n in options:
+    #        print(options)
+    #        board = setNewValueInBoard(board, pointer, n)
+    #        print(board)
+    #        if isBoardComplete(board):
+    #            print(board)
+    #            break
+    #        if isBoardPossible(board):
+    #            pointer += 1
+    #        else:
+    #            print('crap')
+        
+
+#for index,n in enumerate(testBoard):
+#        options = getOptionsByIndex(newBoard, index)
+#        if n=='0':
+#            allOptions.append(options)
+#        else:
+#            allOptions.append([])
+
+#print(allOptions)
+#allOptions[0].append(1)
+#print(allOptions)
+#tryThisCombination(cleanBoard,allOptions)
+
