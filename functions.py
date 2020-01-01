2253459cfe66c67fec596bbb3d1d56f1025bdce3
Daivan Trinh <daivan@gmail.com>
2020-01-01 20:56:23 +0100

    Optmized isBoardPossible

--- functions.py
+++ functions.py
@@ -276,22 +276,23 @@ def fullHiddenSingle(board):
 
 def isBoardPossible(board):
     
+    boxes=getBoxes(board)
+    rows=getRows(board)
+    columns=getColumns(board)
+
     for index, number in enumerate(board):
         if number != 0:
             # check if number already exists in box
-            boxes=getBoxes(board)
             box_values=boxes[getBoxByIndex(index)]
             if box_values.count(number)>1:
                 return False
 
             # check if number already exists in row
-            rows=getRows(board)
             row_values=rows[getRowByIndex(index)]
             if row_values.count(number)>1:
                 return False
 
             # check if number already exists in column
-            columns=getColumns(board)
             column_values=columns[getColumnByIndex(index)]
             if column_values.count(number)>1:
                 return False                
